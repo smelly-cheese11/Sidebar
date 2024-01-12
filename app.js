4c393e1b105a64d071eee975771cb9f015049f40
Caleb <126200524+smelly-cheese11@users.noreply.github.com>
2024-01-12 14:29:01 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const sidebar = document.querySelector('.sidebar')
+const btn = document.querySelector('button')
+
+sidebar.addEventListener('click', () => {
+    sidebar.classList.toggle('hide')
+})
